366f4a1ddc2a5b4a72ca43de30874932521570dd
Handerson Silva <handersonsylva@gmail.com>
2017-07-01 20:01:34 -0300

    Destruindo a sessão do usuario

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -3,6 +3,13 @@ module.exports.jogo=function(application,req,res){
     if(req.session.autorizado){
          res.render('jogo');
     }else{
-        res.send('Usuario precisa fazer login!!!');
+         res.render('index',{validacao:{}});
     }  
+};
+
+module.exports.sair=function(application,req,res){
+   //destruindo a sessão
+   req.session.destroy(function(err){
+        res.render('index',{validacao:{}});
+   })
 };
